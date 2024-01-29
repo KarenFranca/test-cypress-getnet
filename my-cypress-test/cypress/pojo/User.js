8d05a93a433a27355b00e4756aaba0d278b6690d
KarenFranca <157929072+KarenFranca@users.noreply.github.com>
2024-01-29 08:53:30 -0300

    "Entrega Automação GetNet"

--- my-cypress-test/cypress/pojo/User.js
+++ my-cypress-test/cypress/pojo/User.js
@@ -1,17 +1,15 @@
-// Define a classe User (POJO)
-export default class User {
+// User.js
+export class User {
     constructor(name, job) {
         this.name = name;
         this.job = job;
     }
 
-    // Método estático para criar uma instância User a partir de um objeto de resposta
     static fromResponse(responseBody) {
         const { name, job } = responseBody;
         return new User(name, job);
     }
 
-    // Método para converter o POJO em um formato de corpo de requisição
     toRequestBody() {
         return {
             name: this.name,
